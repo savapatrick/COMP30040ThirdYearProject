5cf5e2934aacd2243897d242479865add023cc75
Patrick Catalin Alexandru Sava <svpshal@gmail.com>
2021-04-02 21:18:07 +0100

    literal makes sense

--- project/src/lib/literal.cpp
+++ project/src/lib/literal.cpp
@@ -135,7 +135,6 @@ std::string Literal::getHash(const unordered_map<std::string, std::string>& subs
 }
 
 std::vector<std::string> Literal::getAllVariablesInOrder() const {
-    // todo: maybe here we have to use set?
     vector<string> variablesInOrder;
     for(auto& term : terms) {
         auto currentVariablesInOrder = term->getAllVariablesInOrder();
@@ -145,7 +144,6 @@ std::vector<std::string> Literal::getAllVariablesInOrder() const {
 }
 
 int Literal::getArityExcludingConstants() const {
-    // todo: why do we need them in order?
     auto variablesInOrder = getAllVariablesInOrder();
     return set<string>(variablesInOrder.begin(), variablesInOrder.end()).size();
 }
