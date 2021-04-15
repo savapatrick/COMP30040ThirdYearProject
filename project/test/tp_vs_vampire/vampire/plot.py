d3203e7f3289409e2937285f6f1e0ca5b9e87cbb
Patrick Catalin Alexandru Sava <svpshal@gmail.com>
2021-04-15 22:54:11 +0100

    DONE

--- project/test/tp_vs_vampire/vampire/plot.py
+++ project/test/tp_vs_vampire/vampire/plot.py
@@ -23,6 +23,9 @@ for line in fileinput.input():
         UX.append(int(x))
         UY.append(int(y))
 
+# the next two lines OF CODE are inspired (referenced) 
+# from https://stackoverflow.com/questions/21081453/drawing-scatter-graph-using-matlibplot-pyplot-when-points-are-duplicate
+# accessed on 15th of April 2021
 pyplot.scatter(numpy.array(SX), numpy.array(SY), c='g', marker='s', alpha=.1, s=400)
 pyplot.scatter(numpy.array(UX), numpy.array(UY), c='r', marker='o', alpha=.1, s=400)
 
